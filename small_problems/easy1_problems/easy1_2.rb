6f6cf123ac4208fe76af978beddf7d8a0ed8e4df
Rebecca Biancofiore <rebeccabiancofiore79@gmail.com>
2022-10-19 04:13:43 +0000

    cleaning up files

--- small_problems/easy1_problems/easy1_2.rb
+++ small_problems/easy1_problems/easy1_2.rb
@@ -1,10 +1,11 @@
 #Easy 1 - 2
 
+#10/5/22
 =begin
 Trying written PEDAC here
 P: input - one integer
    output - true if integer is odd
-   rules 
+   rules
    explicit- must define a method
            integer can be positive, negative, or zero
            cannot used .odd?/.even? methods
@@ -19,7 +20,7 @@ E: example1 - input: 7 output: true
 D: data structure - single integer
 A: algorithm - test if integer has a remainder when divided by 2
                if so, output true
-               
+
 C: below
 
 def is_odd?(integer)
@@ -39,6 +40,7 @@ puts is_odd?(101)
 
 # rewriting without module
 
+=begin
 def is_odd?(integer)
   integer.remainder(2) != 0
 end
@@ -53,4 +55,37 @@ puts is_odd?(-8)
 #initially fogot to test for/code for negative number
 #changed > 0 to != 0
 
-               
+=end
+
+#retrying Easy 1.2 on 10/12/22
+=begin
+P: write a method that takes an integer and returns true if number is odd
+
+  input: one integer, can be 0, negative, or positive
+  output: boolean value (true or false)
+
+  explicit rules: assume integer is valid (no validation needed)
+                  input can be positive, negative or zero
+                  return either true (if odd) or false (if not odd)
+  implicit rules: check integer value using boolean set to return true if odd
+                  don't print from within method (call with puts)
+
+E: input: 2 output: false
+  input: 3 output: true
+  input -17 output: true
+  input: 0 output: false
+
+D: working with integers and operators
+
+A: def method is_odd? with one parameter
+   set operation that evaluates to true if integer divided by zero has a
+   remainder
+
+=end
+
+def is_odd?(integer)
+  integer % 2 == 1
+end
+
+puts is_odd?(5)
+puts is_odd?(4)
